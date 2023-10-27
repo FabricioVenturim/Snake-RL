aef8ee9bd0568b2b3060c41fe2ae60b6711c611f
Thiago Franke Melchiors <105611304+TFrankeM@users.noreply.github.com>
2023-10-27 20:35:20 -0300

    Adicione comentários

--- agent.py
+++ agent.py
@@ -1,92 +1,99 @@
 import torch #pytorch
 import random
-import numpy as np #numpy
+import numpy as np
 from collections import deque #data structure to store memory
 from game.snake import SnakeGame, Direction, Point 
 from model.model import Linear_QNet, QTrainer 
 from helper.plot import plot 
 
-MAX_MEMORY = 100_000
-BATCH_SIZE = 1000
-LR = 0.001 #learning rate
+# Constants
+MAX_MEMORY = 100_000  # Maximum memory capacity for agent's experience storage
+BATCH_SIZE = 10000  # Number of experiences used for training in each batch
+LR = 0.001  # Learning rate for the neural network training
 
 class Agent:
     def __init__(self):
-        self.n_games = 0
-        self.epsilon = 0  # randomness
-        self.gamma = 0.9  # discount rate
-        self.memory = deque(maxlen=MAX_MEMORY)  
-        self.model = Linear_QNet(11, 256, 3) #input size, hidden size, output size
-        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+        self.n_games = 0  # Counter for the number of games played
+        self.epsilon = 0  # Exploration rate for making random moves
+        self.gamma = 0.9  # Discount rate for considering future rewards
+        self.memory = deque(maxlen=MAX_MEMORY)  # Storage for the agent's experiences
+        self.model = Linear_QNet(11, 256, 3)  # Neural network model instantiation
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)  # Trainer object for model training
 
     def get_state(self, game):
+        # Extracting the snake's head position and nearby points
         head = game.snake[0]
         point_l = Point(head.x - 20, head.y)
         point_r = Point(head.x + 20, head.y)
         point_u = Point(head.x, head.y - 20)
         point_d = Point(head.x, head.y + 20)
 
+        # Current direction of the snake
         dir_l = game.direction == Direction.LEFT
         dir_r = game.direction == Direction.RIGHT
         dir_u = game.direction == Direction.UP
         dir_d = game.direction == Direction.DOWN
 
+        # Constructing the state from various conditions
         state = [
-            (dir_r and game.is_collision(point_r)) or # Danger straight
+            # Checking if there's danger in the current direction
+            (dir_r and game.is_collision(point_r)) or
             (dir_l and game.is_collision(point_l)) or
             (dir_u and game.is_collision(point_u)) or
             (dir_d and game.is_collision(point_d)),
 
-            (dir_u and game.is_collision(point_r)) or # Danger right
+            # Checking danger to the right
+            (dir_u and game.is_collision(point_r)) or
             (dir_d and game.is_collision(point_l)) or
             (dir_l and game.is_collision(point_u)) or
             (dir_r and game.is_collision(point_d)),
 
-            (dir_d and game.is_collision(point_r)) or # Danger left
+            # Checking danger to the left
+            (dir_d and game.is_collision(point_r)) or
             (dir_u and game.is_collision(point_l)) or
             (dir_r and game.is_collision(point_u)) or
             (dir_l and game.is_collision(point_d)),
 
-            dir_l, #direction
-            dir_r,
-            dir_u,
-            dir_d,
+            dir_l, dir_r, dir_u, dir_d,  # Current direction of the snake
 
-            game.food.x < game.head.x,  # food left
-            game.food.x > game.head.x,  # food right
-            game.food.y < game.head.y,  # food up
-            game.food.y > game.head.y  # food down
+            # Food's relative position
+            game.food.x < game.head.x,  
+            game.food.x > game.head.x,  
+            game.food.y < game.head.y,  
+            game.food.y > game.head.y   
         ]
-        return np.array(state, dtype=int)
-
+        return np.array(state, dtype=int)  # Convert the state to an array and return
 
     def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))  # popleft if MAX_MEMORY is reached
+        # Store the experience in memory
+        self.memory.append((state, action, reward, next_state, done)) 
 
     def train_long_memory(self):
-        if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE)  # list of tuples
-        else:
-            mini_sample = self.memory
-
+        # Train on a batch from the stored experiences
+        mini_sample = random.sample(self.memory, BATCH_SIZE) if len(self.memory) > BATCH_SIZE else self.memory
         states, actions, rewards, next_states, dones = zip(*mini_sample)
         self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short_memory(self, state, action, reward, next_state, done):
+        # Train the model immediately after the agent takes an action
         self.trainer.train_step(state, action, reward, next_state, done)
-    
+
     def get_action(self, state):
-        self.epsilon = 80 - self.n_games
+        # Decide the next action based on epsilon-greedy policy
+        self.epsilon = 80 - self.n_games  # Adjusting exploration rate based on games played
         final_move = [0, 0, 0]
+        
+        # Choose a random action with epsilon probability
         if random.randint(0, 200) < self.epsilon:
             move = random.randint(0, 2)
             final_move[move] = 1
         else:
+            # Otherwise, choose the action with the highest Q-value prediction
             state0 = torch.tensor(state, dtype=torch.float)
             prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
 
-        return final_move
+        return final_move  # Return the chosen action
     
     
